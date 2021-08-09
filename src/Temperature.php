708a3169b1ad0afff656c18b6a300b72ca2d455e
Evingtone Mwailongo <toneevin07@gmail.com>
2021-08-09 08:12:01 +0300

    Add Temperature Conversion

---
+++ src/Temperature.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace EvinNgoa\EvinUnitConversions;
+
+class Temperature
+{
+    private $celsius;
+
+    public static function forCelsius(float $celsius) : self
+    {
+        return new static($celsius);
+    }
+
+    public function __construct(float $celsius)
+    {
+        $this->celsius = $celsius;
+    }
+
+    public function toFahrenheit() : float
+    {
+        return  ($this->celsius * 1.8) + 32;
+    }
+}
